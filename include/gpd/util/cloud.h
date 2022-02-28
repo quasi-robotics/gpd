4d943efa6e0b8f0a656488d8250f888917e8c565
Fedor Bokov <fedor@quasi.ai>
2022-02-28 11:21:40 -0500

    fixed build options

--- include/gpd/util/cloud.h
+++ include/gpd/util/cloud.h
@@ -110,7 +110,7 @@ class Cloud {
      * \return true if they differ in at least one of the first three elements,
      * false otherwise
      */
-    bool operator()(const Eigen::Vector4i &a, const Eigen::Vector4i &b) {
+    bool operator()(const Eigen::Vector4i &a, const Eigen::Vector4i &b) const {
       for (int i = 0; i < a.size() - 1; i++) {
         if (a(i) != b(i)) {
           return true;
